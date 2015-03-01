7087d88d237efc59bd794472595274b7f08aefc6
Stephen Moore <stephen@delfick.com>
2015-03-01 16:56:12 +1100

    Testing SQS.get_all_deployment_messages

--- bespin/amazon/sqs.py
+++ bespin/amazon/sqs.py
@@ -1,4 +1,4 @@
-from bespin.helpers import memoized_property
+from bespin.helpers import memoized_property, until
 from bespin.errors import BadSQSMessage
 
 import boto.sqs
@@ -6,7 +6,6 @@ import boto.sqs
 from collections import namedtuple
 import logging
 import json
-import time
 
 log = logging.getLogger("bespin.amazon.sqs")
 
@@ -30,26 +29,30 @@ class SQS(object):
         return boto.sqs.connect_to_region(self.region)
 
     def get_all_deployment_messages(self, sqs_url, timeout=60, sleep=2):
-        """Get all the messages of the queue, dropping those that are not deployment messages"""
-        start = time.time()
+        """
+        Get all the messages of the queue, dropping those that are not deployment messages
+
+        We keep getting messages whilst the count is greater than 0 and we don't have messages yet.
+
+        We will eventually timeout and return what we have if we keep getting invalid messages or keep getting no messages.
+        """
         messages = []
-
         q = self.conn.get_queue(sqs_url)
-        while q.count() > 0 and not (time.time() - start > timeout):
-            raw_messages = self.conn.receive_message(q, number_messages=1)
+        for _ in until(timeout, step=sleep):
+            while q.count() > 0:
+                raw_messages = self.conn.receive_message(q, number_messages=1)
+                for raw_message in raw_messages:
+                    encoded_message = json.loads(raw_message.get_body())['Message']
 
-            if len(raw_messages) > 0:
-                raw_message = raw_messages[0]
-                encoded_message = json.loads(raw_message.get_body())['Message']
+                    q.delete_message(raw_message)
 
-                q.delete_message(raw_message)
+                    try:
+                        messages.append(Message.decode(encoded_message))
+                    except BadSQSMessage as error:
+                        log.error("Failed to parse a message: %s", error)
 
-                message = Message.decode(encoded_message)
-
-                if message is not None:
-                    messages.append(message)
-
-            time.sleep(sleep)
+            if messages:
+                break
 
         return messages
 
--- tests/amazon/test_sqs.py
+++ tests/amazon/test_sqs.py
@@ -1,9 +1,24 @@
 # coding: spec
 
+from bespin.amazon.sqs import Message, SQS
 from bespin.errors import BadSQSMessage
-from bespin.amazon.sqs import Message
 
 from tests.helpers import BespinCase
+
+import boto
+import mock
+import json
+import nose
+import sys
+
+if sys.version_info[0] == 2 and sys.version_info[1] == 6:
+    # This can be removed when we can use latest Httpretty again
+    def mock_sqs(func):
+        def wrapped(*args):
+            raise nose.SkipTest("No moto support for python2.6 atm")
+        return wrapped
+else:
+    from moto import mock_sqs
 
 describe BespinCase, "Decode Message":
     it "successfully decode a valid message":
@@ -17,3 +32,77 @@ describe BespinCase, "Decode Message":
         raw_message = "success:i-aaaaaaadsfsdfsdontract-628-3b55f412b3016a2b955c49677e89f7e010f7fdc8"
         with self.fuzzyAssertRaisesError(BadSQSMessage, "Less than two colons", msg=raw_message):
             Message.decode(raw_message)
+
+describe BespinCase, "SQS":
+    describe "get_all_deployment_messages":
+        it "times out and returns nothing if we never get any messages":
+            sqs = SQS()
+            conn = mock.Mock(name="conn")
+            conn.get_queue.return_value = mock.Mock(name="queue", count=lambda: 0)
+            sqs_url = mock.Mock(name="sqs_url")
+            sqs.conn = conn
+
+            with mock.patch("time.sleep", lambda amount: None):
+                self.assertEqual(sqs.get_all_deployment_messages(sqs_url, timeout=0), [])
+
+            conn.get_queue.assert_called_once_with(sqs_url)
+
+        it "keeps trying to get messages till it gets some valid ones":
+            sleeps = []
+
+            message1 = mock.Mock(name="message1")
+            message1.get_body.return_value = json.dumps({"Message": "blah"})
+
+            message2 = mock.Mock(name="message2")
+            message2.get_body.return_value = json.dumps({"Message": "nup:"})
+
+            message3 = mock.Mock(name="message3")
+            message3.get_body.return_value = json.dumps({"Message": "success:i-1:blah-9"})
+
+            queue = mock.Mock(name="queue")
+            sqs_url = mock.Mock(name="sqs_url")
+
+            info = {"index": -1, "responses":[[message1], [message2], [message3]]}
+            def receive_message(q, number_messages):
+                self.assertIs(q, queue)
+                self.assertEqual(number_messages, 1)
+                info["index"] += 1
+                return info["responses"][info["index"]]
+
+            conn = mock.Mock(name="conn")
+            conn.get_queue.return_value = queue
+            conn.receive_message.side_effect = receive_message
+
+            queue_info = {"index": -1, "responses": [0, 0, 1, 0, 2, 1, 0]}
+            def count():
+                queue_info["index"] += 1
+                return queue_info["responses"][queue_info["index"]]
+            queue.count.side_effect = count
+
+            def sleep(amount):
+                sleeps.append(amount)
+
+            sqs = SQS()
+            sqs.conn = conn
+            with mock.patch("time.sleep", sleep):
+                self.assertEqual(sqs.get_all_deployment_messages(sqs_url), [Message.decode("success:i-1:blah-9")])
+
+            conn.get_queue.assert_called_once_with(sqs_url)
+            self.assertEqual(sleeps, [2, 2, 2])
+            self.assertEqual(queue.delete_message.mock_calls, [mock.call(message1), mock.call(message2), mock.call(message3)])
+
+        @mock_sqs
+        it "works with the sqs api":
+            conn = boto.connect_sqs()
+            sqs = SQS()
+            sqs.conn = conn
+
+            queue = conn.create_queue("whatever")
+            messages = ["success:i-1:blah", "failure:i-2:blah", "success:i-3:blah", "successi-3blah", "success:i-4:blah"]
+
+            for msg in messages:
+                queue.write(queue.new_message(json.dumps({"Message": msg})))
+
+            found = sqs.get_all_deployment_messages("whatever")
+            self.assertEqual(found, [Message.decode(msg) for msg in messages if msg.count(":") == 2])
+            self.assertEqual(len(found), 4)
