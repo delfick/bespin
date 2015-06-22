dd893c566e7598bacd04f1e2ce8ab1e8686e60c7
Stephen Moore <stephen@delfick.com>
2015-06-22 22:26:53 +1000

    Fixing the tests

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -3,12 +3,11 @@
 from bespin.option_spec.bespin_specs import Bespin, Environment
 from bespin.option_spec.stack_objs import Stack
 from bespin.option_spec.task_objs import Task
-from bespin.overview import Overview
+from bespin.collector import Collector
 
 from tests.helpers import BespinCase
 
 from noseOfYeti.tokeniser.support import noy_sup_setUp
-from option_merge import MergedOptions
 from contextlib import contextmanager
 import mock
 import yaml
@@ -36,7 +35,7 @@ describe BespinCase, "Collecting configuration":
         return location
 
     @contextmanager
-    def make_overview(self, config, home_dir_configuration=None, logging_handler=None, activate_converters=False):
+    def make_collector(self, config, home_dir_configuration=None, activate_converters=False):
         if home_dir_configuration is None:
             if hasattr(self, "home_dir_configuration"):
                 home_dir_configuration = self.home_dir_configuration
@@ -45,35 +44,35 @@ describe BespinCase, "Collecting configuration":
 
         home_dir_configuration_location = mock.Mock(name="home_dir_configuration_location", spec=[])
         home_dir_configuration_location.return_value = home_dir_configuration
-        overview_kls = type("OverviewSub", (Overview, ), {"home_dir_configuration_location": home_dir_configuration_location})
-        overview = overview_kls(config, logging_handler=logging_handler)
+        collector_kls = type("CollectorSub", (Collector, ), {"home_dir_configuration_location": home_dir_configuration_location})
+        collector = collector_kls(config)
         if activate_converters:
-            overview.configuration.converters.activate()
-        yield overview
+            collector.configuration.converters.activate()
+        yield collector
 
     it "includes configuration from the home directory":
         config = self.make_config({"a":1, "b":2, "stacks": {"meh": {}}})
         home_config = self.make_config({"a":3, "c":4})
-        with self.make_overview(config, home_config) as overview:
-            self.assertEqual(sorted(overview.configuration.keys()), sorted(['a', 'b', 'c', 'config_root', 'stacks']))
-            self.assertEqual(overview.configuration['a'], 1)
-            self.assertEqual(overview.configuration['b'], 2)
-            self.assertEqual(overview.configuration['c'], 4)
+        with self.make_collector(config, home_config) as collector:
+            self.assertEqual(sorted(collector.configuration.keys()), sorted(['a', 'b', 'c', 'config_root', 'stacks']))
+            self.assertEqual(collector.configuration['a'], 1)
+            self.assertEqual(collector.configuration['b'], 2)
+            self.assertEqual(collector.configuration['c'], 4)
 
     it "sets up converters for bespin":
         config = self.make_config({"bespin": {}})
-        with self.make_overview(config, activate_converters=True) as overview:
-            self.assertIs(type(overview.configuration["bespin"]), Bespin)
+        with self.make_collector(config, activate_converters=True) as collector:
+            self.assertIs(type(collector.configuration["bespin"]), Bespin)
 
     it "sets up converters for tasks":
         config = self.make_config({"stacks": {"blah": {"resources": [], "tasks": {"a_task": {}}}}})
-        with self.make_overview(config, activate_converters=True) as overview:
-            self.assertIs(type(overview.configuration["stacks.blah.tasks"]["a_task"]), Task)
+        with self.make_collector(config, activate_converters=True) as collector:
+            self.assertIs(type(collector.configuration["stacks.blah.tasks"]["a_task"]), Task)
 
     it "sets up converters for stacks":
         config = self.make_config({"environment": "dev", "environments": {"dev": {}}, "bespin": {"environment": "dev"}, "config_root": ".", "stacks": {"blah": {"params_yaml":self.make_config({"one":"two"}), "stack_json": self.make_config({"Resources": {}}, is_json=True), "resources": []}}})
-        with self.make_overview(config, activate_converters=True) as overview:
-            self.assertIs(type(overview.configuration["stacks.blah"]), Stack)
+        with self.make_collector(config, activate_converters=True) as collector:
+            self.assertIs(type(collector.configuration["stacks.blah"]), Stack)
 
     it "merges environment into stack":
         config = {
@@ -96,22 +95,22 @@ describe BespinCase, "Collecting configuration":
 
         config["environment"] = "dev"
         config["bespin"] = {"environment": "dev"}
-        with self.make_overview(self.make_config(config), activate_converters=True) as overview:
-            stack = overview.configuration["stacks.blah"]
+        with self.make_collector(self.make_config(config), activate_converters=True) as collector:
+            stack = collector.configuration["stacks.blah"]
             self.assertEqual(stack.vars["two"].resolve(), '2')
             self.assertEqual(stack.vars["one"].resolve(), '1')
 
         config["environment"] = "staging"
         config["bespin"] = {"environment": "staging"}
-        with self.make_overview(self.make_config(config), activate_converters=True) as overview:
-            stack = overview.configuration["stacks.blah"]
+        with self.make_collector(self.make_config(config), activate_converters=True) as collector:
+            stack = collector.configuration["stacks.blah"]
             self.assertEqual(stack.vars["two"].resolve(), '2')
             self.assertEqual(stack.vars["one"].resolve(), '2')
 
     it "converts environments":
         config = self.make_config({"environments": {"dev": {"account_id": "1231434"}, "staging": {"account_id": "087089", "vars": {"one": "ONE"}}}})
-        with self.make_overview(config, activate_converters=True) as overview:
-            environment = overview.configuration["environments"]
+        with self.make_collector(config, activate_converters=True) as collector:
+            environment = collector.configuration["environments"]
             self.assertEqual(type(environment["dev"]), Environment)
             self.assertEqual(environment["dev"].account_id, "1231434")
             self.assertEqual(environment["staging"].account_id, "087089")
