f7724de81deeec0dce6def50e7b454ee298cd28a
Stephen Moore <stephen@delfick.com>
2015-06-26 14:12:18 +1000

    Update input_algorithms and delfick-app

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
     , include_package_data = True
 
     , install_requires =
-      [ "delfick_app==0.6"
+      [ "delfick_app==0.6.1"
       , "option_merge==0.9.7"
-      , "input_algorithms==0.4.4.5"
+      , "input_algorithms==0.4.4.6"
 
       , "six"
       , "pytz"
