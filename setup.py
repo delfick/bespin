1509b581788b4b1b515bf602648e399946c03120
Stephen Moore <stephen@delfick.com>
2015-09-02 21:44:40 +1000

    Upgrading option_merge and input_algorithms

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
 
     , install_requires =
       [ "delfick_app==0.6.2"
-      , "option_merge==0.9.8.2"
-      , "input_algorithms==0.4.4.6"
+      , "option_merge==0.9.9.1"
+      , "input_algorithms==0.4.4.7"
 
       , "six"
       , "pytz"
