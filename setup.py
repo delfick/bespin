bb4fb3900a7edadd77ea43b663f58647a68f0a18
Stephen Moore <stephen@delfick.com>
2016-02-08 13:58:58 +1100

    Update option_merge

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
 
     , install_requires =
       [ "delfick_app==0.7.3"
-      , "option_merge==0.9.9.6"
+      , "option_merge==0.9.9.7"
       , "input_algorithms==0.4.5.4"
 
       , "six"
