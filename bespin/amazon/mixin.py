cab9f1472992997551500aa6215b5eb2da7a88eb
Adam Ward <adamw@subdesigns.net>
2017-04-07 14:37:50 +1000

    Migrate to boto3 for cloudformation
    MVP approach to migrating cloudformation calls to boto3.
    More work should be done to migrate to boto3 resources and helpers
    (waiters, pagers).
    
    Adds support for update of stack tags

--- bespin/amazon/mixin.py
+++ bespin/amazon/mixin.py
@@ -2,20 +2,21 @@ from bespin.errors import Throttled
 
 from contextlib import contextmanager
 import logging
-import boto
+import botocore
 
 log = logging.getLogger("bespin.amazon.mixin")
 
 class AmazonMixin(object):
     @contextmanager
     def catch_boto_400(self, errorkls, message, **info):
-        """Turn a BotoServerError 400 into a BadAmazon"""
+        """Turn a boto HTTP 400 into a BadAmazon"""
         try:
             yield
-        except boto.exception.BotoServerError as error:
-            if error.status == 400:
-                log.error("%s -(%s)- %s", message, error.code, error.message)
-                raise errorkls(message, error_code=error.code, error_message=error.message, **info)
+        except botocore.exceptions.ClientError as error:
+            code = error.response['ResponseMetadata']['HTTPStatusCode']
+            if code == 400:
+                log.error("%s -(%s)- %s", message, code, error.message)
+                raise errorkls(message, error_code=code, error_message=error.message, **info)
             else:
                 raise
 
@@ -23,8 +24,8 @@ class AmazonMixin(object):
     def ignore_throttling_error(self):
         try:
             yield
-        except boto.exception.BotoServerError as error:
-            if error.status == 400 and error.code == "Throttling":
+        except botocore.exceptions.ClientError as error:
+            if error.response['Error']['Code'] == 'Throttling':
                 raise Throttled()
             else:
                 raise
